65cb75769cb1909e979f4d9069247097d8f5cbf0
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2022-11-08 18:27:22 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '4a8db82f5aae42aabc027174a522cc28',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Rajib Sarwar',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'rajib-sarwar.transitivebullsh.it',
   author: 'Rajib Sarwar',
 
   // open graph metadata (optional)
